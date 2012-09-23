18bdc734109030868370c299eac1460091d23ef8
Yunus Durmus <yunusd84@gmail.com>
2012-09-23 16:09:35 +0200

    transmission unit and message were using same aliases

--- bin/explorer.rb
+++ bin/explorer.rb
@@ -9,7 +9,7 @@ class Explorer < Thor
   method_option :profile, :required=>true, :aliases => "-p",:type => :string,
     :desc => "The absolute address of advertisement profile file of the device. It includes all the device, service and social network information"
   method_option :max_transmission_unit, :default => 1500, :type => :numeric,
-    :aliases => "-m", :desc => "Depending on the interface type different MTU values are required."
+    :aliases => "-n", :desc => "Depending on the interface type different MTU values are required."
   method_option :cache_path, :default => "./", :type => :string, :aliases => "-c",
     :desc => "The advertisements of the devices are cached in the given directory."
   method_option :log_file, :default => "STDOUT", :aliases => "-l",
@@ -22,7 +22,7 @@ class Explorer < Thor
     log_level(options)
     
     Scd::Listener.listen_advertisements(multicast_group_address, 
-      options[:cache_path], options[:profile],options[:max_transmission_unit])
+      options[:cache_path], options[:profile],Float(options[:max_transmission_unit]))
     
   end
 
@@ -36,7 +36,7 @@ class Explorer < Thor
   method_option :solicitation, :boolean => false, :aliases => "-s",
     :desc => "Message type is set to solicitation. Otherwise advertisement is sent."
   method_option :max_transmission_unit, :default => 1500, :type => :numeric,
-    :aliases => "-m", :desc => "Depending on the interface type different MTU values are required."
+    :aliases => "-n", :desc => "Depending on the interface type different MTU values are required."
   method_option :log_file, :default => "STDOUT", :aliases => "-l",
     :desc=> "The name of the log file"
   method_option :debug, :boolean => true,:aliases => "-d",
@@ -50,9 +50,9 @@ class Explorer < Thor
     type_class = options[:solicitation] ? Scd::ICMPv6::Solicitation : Scd::ICMPv6::Advertisement
 
     Scd::Publisher.pub_file(options[:file], multicast_group_address, type_class,
-      options[:max_transmission_unit])  unless options[:file].nil?
+      Float(options[:max_transmission_unit]))  unless options[:file].nil?
     Scd::Publisher.pub_message(options[:message], multicast_group_address, type_class,
-      options[:max_transmission_unit])  unless options[:message].nil?
+      Float(options[:max_transmission_unit]))  unless options[:message].nil?
 
   end
 
